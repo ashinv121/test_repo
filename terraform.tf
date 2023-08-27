c086b3107037f5bcc952fcddc2941c77baa4fc1a
Ashin v <37022429+ashinv121@users.noreply.github.com>
2023-08-27 11:57:31 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -11,8 +11,12 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-north-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   
 }
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-0989fb15ce71ba39e"
